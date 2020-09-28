39408d6a58dc9688207912b32797bf01759359ff
julio marin <jotacemarin@gmail.com>
2020-09-27 19:57:43 -0500

    added validation in best scores list

--- src/views/BestScores.vue
+++ src/views/BestScores.vue
@@ -3,18 +3,31 @@
         <h1>This is an best scores page</h1>
 
         <v-list v-if="!loading">
-            <v-list-item v-for="(item, i) in bestScores"
-                :key="i"
-                two-line
-            >
-                <v-list-item-icon>
-                    <v-icon>{{ retrieveIcon(i) }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                    <v-list-item-title>{{ item.nickname }}</v-list-item-title>
-                    <v-list-item-subtitle>{{ item.score }}</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
+            <template v-if="bestScores.length > 0">
+                <v-list-item v-for="(item, i) in bestScores"
+                    :key="i"
+                    two-line
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ retrieveIcon(i) }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.nickname }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ item.score }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
+            <template v-else>
+                <v-list-item>
+                    <v-list-item-icon>
+                        <v-icon>mdi-circle-off-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>Empty list</v-list-item-title>
+                        <v-list-item-subtitle>Without results</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
         </v-list>
         <v-skeleton-loader v-else type="list-item-avatar-two-line" class="mx-auto" />
 
@@ -39,6 +52,7 @@ export default {
 
     methods: {
         retrieveIcon(index) {
+            [].length
             return index < 3
                 ? `mdi-numeric-${index + 1}-circle-outline`
                 : `mdi-circle-outline`;
